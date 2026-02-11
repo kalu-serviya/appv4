f5fa1cd66de1da3310398278f5d676cf03101819
ServiWork <2025serviya@gmail.com>
2026-02-10 21:13:22 -0300

    Add files via upload

---
+++ app/build/generated/source/buildConfig/debug/servi/work/BuildConfig.java
@@ -0,0 +1,14 @@
+/**
+ * Automatically generated file. DO NOT MODIFY
+ */
+package servi.work;
+
+public final class BuildConfig {
+  public static final boolean DEBUG = Boolean.parseBoolean("true");
+  public static final String APPLICATION_ID = "servi.work";
+  public static final String BUILD_TYPE = "debug";
+  public static final int VERSION_CODE = 1;
+  public static final String VERSION_NAME = "1.0";
+  // Field from default config.
+  public static final String RESEND_API_KEY = "re_NojXK48h_9i1QABxuRpXYG3aCsPwzfke2";
+}
